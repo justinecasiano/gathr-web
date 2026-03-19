4c6da70f9cdef7bf83700647c74f2478dab6ca5a
Justine Casiano <justinecasiano30@gmail.com>
2026-03-19 17:03:00 +0800

    chore: add initial files and routes

---
+++ app/organizer/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+
+export default function SignInPage() {
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className="text-blue-500">Welcome to Sign-in</h1>
+        </div>
+    );
+}
