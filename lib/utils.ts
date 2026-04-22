690d2f335458dedf431a21a51cf8dfc10f474ab9
Justine Casiano <justinecasiano30@gmail.com>
2026-04-23 03:50:19 +0800

    fix: vercel site url not being used for reset password email

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,10 +6,10 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const getURL = () => {
-    let url =
-        process?.env?.NEXT_PUBLIC_SITE_URL ??
-        process?.env?.NEXT_PUBLIC_VERCEL_URL ??
-        'http://localhost:3000/';
+    const envUrl = process.env.NEXT_PUBLIC_SITE_URL;
+
+    let url = envUrl ||
+        (typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000');
 
     url = url.includes('http') ? url : `https://${url}`;
     url = url.charAt(url.length - 1) === '/' ? url.slice(0, -1) : url;
